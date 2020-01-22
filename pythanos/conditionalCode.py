2f1e0568dabb4be7e3df85a2c9151d7c59535649
Nitin Reddy <reddynitin3@gmail.com>
2020-01-22 17:50:43 +0530

    Studied the loops

---
+++ pythanos/conditionalCode.py
@@ -0,0 +1,8 @@
+def checkIfPositive(number):
+    if number >= 0:
+        return "Number is positive"
+    else:
+        return "Number is negative"
+
+
+print(checkIfPositive(0))
---
+++ pythanos/forLoop.py
@@ -0,0 +1,12 @@
+list_object = [
+    {id: 'gsdf'},
+    {id:  'sdfg'}
+]
+
+
+def loop_through(list_item):
+    for n in list_item:
+        print("Item key: " + n.id)
+
+
+loop_through(list_object)
